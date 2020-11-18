a3100857903ba938b9d46af688ec6a0a8aba0f94
MrBellamonte <schsimo@ethz.ch>
2020-11-18 19:05:00 +0100

    version 0.0.1

--- setup.py
+++ setup.py
@@ -35,5 +35,5 @@ setuptools.setup(
     description='Witness complex construction package.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
